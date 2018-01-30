123425d05488ca48a63e27aed76bf8583c4db48a
Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
2018-01-30 14:16:41 +0100

    Updating submodules
    Signed-off-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- update_submodules.sh
+++ update_submodules.sh
@@ -2,10 +2,5 @@
 
 GIT=/usr/bin/git
 
-# Init and update submodules
-echo "Submodule init..."
-$GIT submodule init
-echo "Submodule update..."
-$GIT submodule update
-echo "Submodule updating each repo..."
+$GIT submodule update --init --recursive
 $GIT submodule foreach git pull origin master
